ccfc1754aa6e78159c8e3719a0fe91817016d3b1
Chris Jardine <c_jardine@icloud.com>
2021-10-26 16:35:04 -0400

    Show/hide the form for adding a new tray.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import Trays from "./components/Trays";
+import Button from "./components/Button";
+import AddTray from "./components/AddTray";
 
 function App() {
+  const [showAddTray, setShowAddTray] = useState(false);
+
   const [trays, setTrays] = useState([
     {
       id: 1,
@@ -16,9 +20,20 @@ function App() {
       title: "Done",
     },
   ]);
-  
+
+  const addTray = (tray) => {
+    const id = Math.floor(Math.random() * 1000 + 1);
+    const newTray = { id, ...tray };
+    setTrays([...trays, newTray]);
+  };
+
   return (
     <div className="application">
+      {/* Button to show the form to add a new tray. */}
+      <Button text="Add Tray" onAdd={() => setShowAddTray(!showAddTray)} />
+      {showAddTray && <AddTray onAdd={addTray} />}
+
+      {/* Display all available trays. */}
       {trays.length > 0 ? <Trays trays={trays} /> : "No trays to show."}
     </div>
   );
---
+++ src/components/Button.js
@@ -0,0 +1,6 @@
+const Button = ({ text, onAdd }) => {
+    return (
+        <button onClick={onAdd}>{text}</button>
+    )
+}
+export default Button
